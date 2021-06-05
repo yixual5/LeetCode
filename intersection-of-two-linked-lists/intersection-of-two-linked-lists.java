a08912b0f682c14eb7c5ee74e8f291b31b6d5174
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-05 16:06:06 -0700

    Update intersection-of-two-linked-lists.java

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -9,7 +9,7 @@
  *     }
  * }
  */
-// update
+// 40mins to come up with this optimal solution
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) return null;
