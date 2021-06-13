ca051ce4c8b7770593c0a7bd36fa09dc819b39d4
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-12 20:51:39 -0700

    Time: 1369 ms (5.03%), Space: 181.7 MB (5.65%) - LeetHub

---
+++ combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        LinkedList<Integer> cur = new LinkedList<>();
+        backtracking(0, target, candidates, result, cur);
+        return result;
+    }
+    
+    private void backtracking (int idx, int remain, int[] candidates, List<List<Integer>> result,
+                              LinkedList<Integer> cur) {
+        if (remain == 0) {
+            if (!result.contains(cur)) result.add((LinkedList) cur.clone());
+            
+            return;
+        } else if (remain < 0) {
+            return;
+        }
+        for (int i = idx; i < candidates.length && candidates[i] <= remain; ++i) {
+            cur.add(candidates[i]);
+            backtracking(i + 1, remain - candidates[i], candidates, result, cur);
+            cur.removeLast();
+        }
+    }
+}
