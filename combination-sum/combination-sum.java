2cd5296fd911b06f9610359414d57b10048185b9
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-12 19:14:37 -0700

    Time: 7 ms (19.81%), Space: 44.8 MB (5.23%) - LeetHub

---
+++ combination-sum/combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    private List<List<Integer>> result;
+    int[] candidates;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        this.result = new ArrayList<>();
+        this.candidates = candidates;
+        if (candidates == null) return this.result;
+        count(new LinkedList<Integer>(), 0, target, 0);
+        return result;
+    }
+    
+    private List<Integer> count(LinkedList<Integer> cur, int sum, int target, int idx) {
+        
+        if (sum == target) return cur;
+        if (sum > target) return null;
+        for (int i = idx; i < candidates.length; i++) {
+            cur.add(candidates[i]);
+            
+            if (count(cur, sum + candidates[i], target, i) != null) {
+                result.add((LinkedList) cur.clone());
+            }
+            cur.removeLast();
+        }
+        return null; // never reach here because we can also keep add some values
+    }
+}
