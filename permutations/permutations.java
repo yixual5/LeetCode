452693ccd150b9487f1e2322313f753004bb9ab0
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-12 23:01:09 -0700

    Time: 1 ms (93.02%), Space: 39.5 MB (28.29%) - LeetHub

---
+++ permutations/permutations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums == null) return null;
+        List<List<Integer>> result = new ArrayList<>();
+        LinkedList<Integer> cur = new LinkedList<>();
+        
+        backtracking(result, cur, nums);
+        return result;
+    }
+    
+    private void backtracking(List<List<Integer>> result, LinkedList<Integer> cur,                                      int[] nums){
+        if (cur.size() == nums.length) {
+            result.add((LinkedList) cur.clone());
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (cur.contains(nums[i])) continue;
+            cur.add(nums[i]);
+            backtracking(result, cur, nums);
+            cur.removeLast();
+        }
+    }
+}
