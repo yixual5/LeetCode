57b5b48b36ecec3597017abaeff7166174781cf1
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-13 20:40:14 -0700

    20 mins, taking while for debug

--- path-sum/path-sum.java
+++ path-sum/path-sum.java
@@ -29,3 +29,15 @@ class Solution {
             
     }
 }
+
+
+// concise version
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return targetSum - root.val == 0;
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+    
+    
+}
