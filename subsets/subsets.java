7c2cc4fce98e7d5449c5e4fbb8daa48952b91dc2
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-13 20:07:11 -0700

    Time: 0 ms (100.00%), Space: 39.2 MB (50.43%) - LeetHub

---
+++ subsets/subsets.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) return result;
+        LinkedList<Integer> cur = new LinkedList<>();
+        int idx = 0;
+        backtracking(nums, result, cur, idx);
+        return result;
+    }
+    
+    private void backtracking (int[] nums, List<List<Integer>> result,                  LinkedList<Integer> cur, int idx) {
+        result.add((LinkedList) cur.clone());
+        if (idx == nums.length) return;
+        for (int i = idx; i < nums.length; ++i) {
+            cur.add(nums[i]);
+            backtracking(nums, result, cur, i + 1);
+            cur.removeLast();
+        }
+    }
+}
