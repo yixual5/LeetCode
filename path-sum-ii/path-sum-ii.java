4b9730b27a45df7be255784aad18fbb2c62d1d0a
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-13 21:46:14 -0700

    Time: 1 ms (99.93%), Space: 39.3 MB (71.85%) - LeetHub

---
+++ path-sum-ii/path-sum-ii.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        LinkedList<Integer> cur = new LinkedList<>();
+        
+        backtracking(root, targetSum, result, cur);
+        return result;
+    }
+    
+    private void backtracking(TreeNode root, int targetSum, List<List<Integer>> result,
+                             LinkedList<Integer> cur) {
+        if (root == null) return;
+        
+        if (root.left == null && root.right == null && targetSum - root.val == 0) {
+            cur.add(root.val);
+            result.add((LinkedList) cur.clone());
+            cur.removeLast();
+            return;
+        }
+        //
+        cur.add(root.val);
+        backtracking(root.left, targetSum - root.val, result, cur);
+        backtracking(root.right, targetSum - root.val, result, cur);
+        cur.removeLast();
+    }
+}
