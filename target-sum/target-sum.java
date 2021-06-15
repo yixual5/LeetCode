aabc5cbf5c69d1a1fcdeb8e4d99fc7a050cb18ef
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-14 19:35:18 -0700

    Time: 516 ms (13.89%), Space: 36.3 MB (93.67%) - LeetHub

---
+++ target-sum/target-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    private int count;
+    
+    public int findTargetSumWays(int[] nums, int target) {
+        if (nums == null) return 0;
+        this.count = 0;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        Arrays.sort(nums);
+        backtracking(nums, target, 0, sum);
+        return this.count;
+    }
+    
+    private void backtracking(int[] nums, int target, int idx, int sum) {
+        
+        if (sum == target) {
+            this.count++;
+            if (idx != 0 && nums[idx - 1] != 0) return;
+            
+        }
+        
+                
+        for (int i = idx; i < nums.length && sum + 2*nums[i] >= target; ++i) {
+            nums[i] = - nums[i];
+            
+            backtracking(nums, target, i + 1, sum + 2*nums[i]);
+            nums[i] = - nums[i];
+        }
+    }
+}
