8511ce1f5207aa0723d03bfbe353979713fd062d
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-16 00:24:57 -0700

    30 mins

--- subsets-ii/subsets-ii.java
+++ subsets-ii/subsets-ii.java
@@ -1,8 +1,9 @@
 class Solution {
+    // take some time to think about how to skip the duplicate subset
     public List<List<Integer>> subsetsWithDup(int[] nums) {
 
         List<List<Integer>> result = new ArrayList<>();
-        if (nums == null) return result;
+        if (nums == null || nums.length == 0) return result;
         LinkedList<Integer> cur = new LinkedList<>();
         int idx = 0;
         Arrays.sort(nums);
@@ -10,10 +11,11 @@ class Solution {
         return result;
     }
     
-    private void backtracking (int[] nums, List<List<Integer>> result,                  LinkedList<Integer> cur, int idx) {
+    private void backtracking (int[] nums, List<List<Integer>> result, LinkedList<Integer> cur, int idx) {
         result.add((LinkedList) cur.clone());
         if (idx == nums.length) return;
         for (int i = idx; i < nums.length; ++i) {
+            // if we alreadt not consider i -1, we should not consider i (same value) in the same level. which needs i > idx
             if (i > idx && nums[i] == nums[i - 1]) continue;
             cur.add(nums[i]);
             backtracking(nums, result, cur, i + 1);
