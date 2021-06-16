3dbe5e9b52cf884ea21fd828591982b93710a74a
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-15 21:31:03 -0700

    Time: 8 ms (54.38%), Space: 51.5 MB (27.45%) - LeetHub

---
+++ increasing-subsequences/increasing-subsequences.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) return result;
+        LinkedList<Integer> cur = new LinkedList<>();
+        backtracking(0, nums, result, cur);
+        
+        return result;
+    }
+    
+    private void backtracking(int idx, int[] nums, List<List<Integer>> result,       LinkedList<Integer> cur) {
+        
+        if (idx > nums.length) return;
+        
+        
+        if (cur.size() >= 2) {
+            result.add((LinkedList) cur.clone());
+        }
+        Set<Integer> visited = new HashSet<>();
+        for (int i = idx; i < nums.length; i++) {
+            if (visited.contains(nums[i])) continue;
+            if (cur.size() > 0 && cur.get(cur.size() - 1) > nums[i]) continue; 
+            visited.add(nums[i]);
+            cur.add(nums[i]);
+            backtracking(i + 1, nums, result, cur);
+            cur.removeLast();
+        }
+    }
+}
