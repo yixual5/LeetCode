0ed5cdbd3d33fae571d07b0cf485f97ba42f381b
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-17 03:44:49 -0700

    top-down solution 20 mins

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -13,6 +13,33 @@
  *     }
  * }
  */
+
+// initial solution top-down
+class Solution {
+    private int mxdepth = 0;
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        dfs(root, 0);
+        return mxdepth;
+    }
+    
+    private void dfs(TreeNode root, int mlength) {
+        mlength++;
+        if (root.left == null && root.right == null) {
+            mxdepth = Math.max(mlength, mxdepth);
+            return;
+        }
+        
+        if (root.left != null) {
+            dfs(root.left, mlength);
+        }
+        if (root.right != null) {
+            dfs(root.right, mlength);
+        }
+    }
+}
+
+//concise solution botton-up
 class Solution {
     
     public int maxDepth(TreeNode root) {
