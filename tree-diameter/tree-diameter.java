7cbd2b0240ab3112d97b3718d839ac4cc952d026
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-20 19:02:46 -0700

    Time: 11 ms (64.30%), Space: 42.7 MB (50.86%) - LeetHub

---
+++ tree-diameter/tree-diameter.java
@@ -0,0 +1,39 @@
+class Solution {
+    private int lent = 0;
+    private Map<Integer, List<Integer>> count = new HashMap<>();
+    
+    public int treeDiameter(int[][] edges) {
+        if (edges.length == 0) return 0;
+        
+        
+        for (int[] edge : edges) {
+            count.computeIfAbsent(edge[0], k -> new LinkedList<>()).add(edge[1]);
+            count.computeIfAbsent(edge[1], k -> new LinkedList<>()).add(edge[0]);
+        }
+        boolean[] visited = new boolean[count.keySet().size()];
+        //visited[0] = true;
+        dfs(0, visited);
+        return lent;
+    }
+    
+    private int dfs (int curNode, boolean[] visited) {
+        //if (visited[curNode]) return 0;
+        visited[curNode] = true;
+        int max1 = 0;
+        int max2 = 0;
+        for (Integer i : count.get(curNode)){
+            
+            if (visited[i]) continue;
+            int temp = 1 + dfs(i, visited);
+            if (temp > max1) {
+                max2 = max1;
+                max1 = temp;
+            } else if (temp > max2) {
+                max2 = temp;
+            }
+        }
+        lent = Math.max(lent, max1 + max2);
+        return max1;
+        
+    }
+}
