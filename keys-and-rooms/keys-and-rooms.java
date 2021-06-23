ea1ee10ee0f36d91b2ef7f9905603e5f43cc9045
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-22 22:16:48 -0700

    Time: 1 ms (80.80%), Space: 38.9 MB (52.01%) - LeetHub

---
+++ keys-and-rooms/keys-and-rooms.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        if (rooms == null || rooms.size() == 0) return true;
+        boolean[] visited = new boolean[rooms.size()];
+        visited[0] = true;
+        Stack<Integer> cur = new Stack<>();
+        cur.push(0);
+        
+        while (!cur.isEmpty()) {
+            int temp = cur.pop();
+            for (Integer i : rooms.get(temp)) {
+                if (visited[i]) continue;
+                cur.push(i);
+                visited[i] = true;
+            }
+        }
+        
+        for (boolean b : visited) {
+            if (!b) return false;
+        }
+        return true;
+    }
+}
