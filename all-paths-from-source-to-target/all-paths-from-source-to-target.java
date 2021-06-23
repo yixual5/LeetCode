0fab1f5417fc4a163f76d3b9c75ff5d53e562400
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-22 23:41:40 -0700

    Time: 4 ms (33.90%), Space: 42.7 MB (21.35%) - LeetHub

---
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (graph == null || graph.length == 0) return result;
+        LinkedList<Integer> cur = new LinkedList<>();
+        cur.add(0);
+        backtracking(result, cur, 0, graph.length - 1, graph);
+        return result;
+    }
+    
+    private void backtracking(List<List<Integer>> result, LinkedList<Integer> cur, int node, int target,
+                             int[][] graph) {
+        if (node == target) {
+            result.add((LinkedList) cur.clone());
+            return;
+        }
+        for (int i : graph[node]) {
+            cur.add(i);
+            backtracking(result, cur, i, target, graph);
+            cur.removeLast();
+        }
+    }
+}
