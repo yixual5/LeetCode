bfadbe2355b7130a3649abdc7e28b7e4af0be650
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-24 00:50:23 -0700

    40 mins

--- count-servers-that-communicate/count-servers-that-communicate.java
+++ count-servers-that-communicate/count-servers-that-communicate.java
@@ -26,3 +26,35 @@ class Solution {
         return total;
     }
 }
+
+//one pass
+class Solution {
+    // two pass
+    public int countServers(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        int total = 0;
+        int ca = 0;
+        for (int i = 0; i < row; i++) {
+            int count = 0;
+            int idx = 0;
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    idx = j;
+                    count++;
+                    total++;
+                }
+            }
+            if (count == 1) {
+                for (int k = 0; k < row; k++) {
+                    if (grid[k][idx] == 1) count++;
+                    
+                }
+                if (count == 2) ca++;
+            }
+        }
+        
+        return total - ca;
+    }
+}
