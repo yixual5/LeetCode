d85b3cb392c02d8a3a6a388ab76bfac43f20d8ca
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-25 04:34:05 -0700

    30 mins

--- read-n-characters-given-read4/read-n-characters-given-read4.java
+++ read-n-characters-given-read4/read-n-characters-given-read4.java
@@ -29,3 +29,29 @@ public class Solution extends Reader4 {
         return count;
     }
 }
+
+//cleaner sol
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char[] buf4);
+ */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    public int read(char[] buf, int n) {
+        char[] temp = new char[4];
+        int readL = 4; int count = 0;
+        while (count < n && readL == 4) {
+            readL = read4(temp);
+            for (int i = 0; i < readL; i++) {
+                buf[count++] = temp[i];
+                if (count == n) return count;
+            }
+        }
+        return count;
+    }
+}
