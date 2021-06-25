2a6668d220a145a1439dff305673138346b698e2
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-25 04:27:51 -0700

    Time: 0 ms (100.00%), Space: 37.3 MB (29.87%) - LeetHub

---
+++ read-n-characters-given-read4/read-n-characters-given-read4.java
@@ -0,0 +1,31 @@
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char[] buf4);
+ */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    public int read(char[] buf, int n) {
+        char[] temp = new char[4];
+        int itr = n % 4 == 0 ? n / 4 : n / 4 + 1;
+        
+        int count = 0;
+        for (int i = 0; i < itr; i++) {
+            int curc = read4(temp);
+            count += curc;
+            for (int j = 0; j < curc; j++) {
+                if (i * 4 + j == n) {
+                    
+                    return n;
+                }
+                buf[i * 4 + j] = temp[j];
+            }
+            if (curc < 4) return count;
+        }
+        return count;
+    }
+}
