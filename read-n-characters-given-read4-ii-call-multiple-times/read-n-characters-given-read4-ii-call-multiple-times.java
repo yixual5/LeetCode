8df7ffab94694dae1e9836f2e5f081ee87bb2f52
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-25 19:59:11 -0700

    40 mins

--- read-n-characters-given-read4-ii-call-multiple-times/read-n-characters-given-read4-ii-call-multiple-times.java
+++ read-n-characters-given-read4-ii-call-multiple-times/read-n-characters-given-read4-ii-call-multiple-times.java
@@ -9,41 +9,30 @@ public class Solution extends Reader4 {
      * @param n   Number of characters to read
      * @return    The number of actual characters read
      */
-    private int remain;
+    private int remain = 0;
     private char[] temp;
-    private int prevCount = 0;
     private int readL = 4;
     public int read(char[] buf, int n) {
         int count = 0;
-        if (temp != null && remain != 0) {
-            
+        if (remain != 0) {
             while (remain < readL && count < n) {
-                
                 buf[count++] = temp[remain++];
-                
                 if (count == n) {
-                    
                     return count;
                 }
             }
-            
         }
-        
         temp = new char[4];
-        
         while (count < n && readL == 4) {
             readL = read4(temp);
             for (int i = 0; i < readL; i++) {
                 buf[count++] = temp[i];
                 if (count == n) {
-                    
                     remain = i + 1;
                     return count;
                 } 
             }
         }
-        remain = 0;
-        
         return count;
     }
 }
