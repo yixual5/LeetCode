3602395461b5eea34721296c1ddb168084349e9d
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-26 20:20:36 -0700

    Time: 2 ms (95.77%), Space: 42.5 MB (6.37%) - LeetHub

---
+++ meeting-rooms-ii/meeting-rooms-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) return 0;
+        if (intervals.length == 1) return 1;
+        int lent = intervals.length;
+        int[] start = new int[lent];
+        int[] end = new int[lent];
+        int idx = 0;
+        for (int[] i : intervals) {
+            start[idx] = i[0];
+            end[idx++] = i[1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int p1;
+        int p2 = 0;
+        int count = 0;
+        for (p1 = 0; p1 < lent; p1++) {
+            if (start[p1] < end[p2]) {
+                count++;
+            } else {
+                p2++;
+            }
+            
+        }
+        return count;
+    }
+}
