fbaf03a3e0b3d7e9240d7b054576d26fa41b0ce6
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-27 02:14:57 -0700

    25 mins

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -12,3 +12,25 @@ class Solution {
         return -1;
     }
 }
+
+//compare by two pointer
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null) return -1;
+        if (needle.length() == 0) return 0;
+        
+        int j = 0;
+        int hlent = haystack.length();
+        int nlent = needle.length();
+        char[] h = haystack.toCharArray();
+        char[] n = needle.toCharArray();
+        for (int i = 0; i + nlent <= hlent; i++) {
+            while (j < nlent && h[i + j] == n[j]) {
+                j++;
+                if (j == nlent) return i;
+            }
+            j = 0;
+        }
+        return -1;
+    }
+}
