976d0a593cee6b60c11056f15e90e5c3b83a8f7e
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-28 06:42:38 -0700

    1 hr and 15 mins

--- robot-bounded-in-circle/robot-bounded-in-circle.java
+++ robot-bounded-in-circle/robot-bounded-in-circle.java
@@ -1,3 +1,10 @@
+// if after first round we back to origin, it means all movements are cancelled with each other, we will still back to origin in the future rounds, which result
+// in a cycle. If after first round we point to north(0), we will keep move same amount as before. As long as we did not back to origin at the first round, we keep
+// moving and never back. If we are 90 degree off from north, we are guaranteed back to origin in 4 rounds because we move same distance each round with
+// different direction. After 4 rounds, we move same amount in each direction, which cancels each other - back to origin. if 180 degree off, we only need two rounds
+// to check if in cycle or not, we can check whether back to origin or face to north.
+
+
 class Solution {
     private int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     public boolean isRobotBounded(String instructions) {
