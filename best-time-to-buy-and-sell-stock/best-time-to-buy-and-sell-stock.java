52f4560d24e10dfbb964a4bc6a151fd564f776e6
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-29 02:56:23 -0700

    30 mins, not figure it out first

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -10,3 +10,23 @@ class Solution {
         return maxprofit;
     }
 }
+
+// avoid duplicate computation
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (minprice > prices[i]) {
+                minprice = prices[i];
+            } else {
+                maxprofit = Math.max(maxprofit, prices[i] - minprice);
+            }
+            
+        }
+        return maxprofit;
+    }
+}
+
+//Note also can be converted to the maximum subarray problem LC.53. As long as you gain profit, you can hold the stock.
