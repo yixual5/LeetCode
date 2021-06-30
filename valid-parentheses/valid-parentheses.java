6e3385d4d89f9bb78896d4ba60139bb2b26332a3
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-30 05:29:22 -0700

    30 mins

--- valid-parentheses/valid-parentheses.java
+++ valid-parentheses/valid-parentheses.java
@@ -1,3 +1,4 @@
+// intuitive solution, using stack
 class Solution {
     public boolean isValid(String s) {
         if (s == null || s.length() == 0) return true;
@@ -20,5 +21,37 @@ class Solution {
         }
         if (!ss.isEmpty()) return false;
         return true;
+    }
+}
+
+//save some space
+class Solution {
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) return true;
+        if (s.length() % 2 != 0) return false;
+        Stack<Character> ss = new Stack<>();
+        /*Map <Character, Character> m = new HashMap<>();
+        m.put('(', ')');
+        m.put('{', '}');
+        m.put('[', ']');*/
+        
+        for (int i = 0; i < s.length(); i++) {
+            char temp = s.charAt(i);
+            if (temp == '(' || temp == '{' || temp == '[') {
+                ss.push(temp);
+            } else {
+                if (ss.isEmpty()) {
+                    return false;
+                }
+                if ((ss.peek() + 2 != temp && ss.peek() + 1 != temp)) {
+                    ss.pop();
+                    return false;
+                }
+                ss.pop();
+            }
+            
+        }
+        
+        return ss.isEmpty();
     }
 }
