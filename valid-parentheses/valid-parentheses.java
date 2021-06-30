a4dcf757637b99d6f688fe72864992576f6c5e06
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-30 04:48:39 -0700

    Time: 2 ms (32.04%), Space: 37.2 MB (44.04%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) return true;
+        if (s.length() % 2 != 0) return false;
+        Stack<Character> ss = new Stack<>();
+        ss.push(s.charAt(0));
+        Set<Character> l = new HashSet<>(Arrays.asList('(', '{', '['));
+        Set<Character> r = new HashSet<>(Arrays.asList(')', '}', ']'));
+        
+        for (int i = 1; i < s.length(); i++) {
+            char temp = s.charAt(i);
+            if (l.contains(temp)) {
+                ss.push(temp);
+            } else {
+                if (ss.isEmpty()) return false;
+                char temp2 = ss.pop();
+                if (temp2 + 2 != temp && temp2 + 1 != temp) return false;
+            }
+            
+        }
+        if (!ss.isEmpty()) return false;
+        return true;
+    }
+}
