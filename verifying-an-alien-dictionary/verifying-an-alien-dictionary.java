f85fb8b7a9ab9431d3cd4346045feb2ea77d58b3
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-06-30 04:23:52 -0700

    25 mins

--- verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
+++ verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
@@ -1,11 +1,12 @@
+// intuitive sol
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
         if (words == null || words.length == 0) return true;
         char[] ord = order.toCharArray();
-        Map<Character, Integer> m = new HashMap<>();
+        Map<Character, Integer> m = new HashMap<>(); // int[] m = new int[26];
         int j = 0;
         for (char c : ord) {
-            m.put(c, j++);
+            m.put(c, j++); // m[c - 'a'] = j++;
         }
         
         for (int i = 1; i < words.length; i++) {
@@ -19,7 +20,7 @@ class Solution {
                     k++;
                     continue;
                 }
-                if (m.get(temp1[k]) < m.get(temp2[k])) {
+                if (m.get(temp1[k]) < m.get(temp2[k])) { // m[temp1[k] - 'a'] < m[temp2[k] - 'a']
                     return false;
                 }
                 b = true;
@@ -30,3 +31,5 @@ class Solution {
         return true;
     }
 }
+
+//
