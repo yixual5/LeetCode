5c880f42105563173b1970f3792a022bc31cf7ea
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-07-02 03:48:32 -0700

    1 hr

--- find-median-from-data-stream/find-median-from-data-stream.java
+++ find-median-from-data-stream/find-median-from-data-stream.java
@@ -39,4 +39,34 @@ class MedianFinder {
     }
 }
 
+// two heap, reduce the offer and poll operation by 2
+class MedianFinder {
+    Queue<Integer> minheap = new PriorityQueue<>((q1,q2) -> q1 - q2);
+    Queue<Integer> maxheap = new PriorityQueue<>((q1, q2) -> q2 - q1);
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        
+    }
+    
+    public void addNum(int num) {
+        
+        if (!maxheap.isEmpty() && maxheap.peek() < num) {
+            minheap.offer(num);
+        } else {
+            maxheap.offer(num);
+        }
+        if (maxheap.size() - minheap.size() == 2) {
+            minheap.offer(maxheap.poll());
+        } else if (maxheap.size() - minheap.size() == -2){
+            maxheap.offer(minheap.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if (maxheap.size() == minheap.size()) return (double) (maxheap.peek() + minheap.peek()) / 2;
+        if (maxheap.size() < minheap.size()) return minheap.peek();
+        return maxheap.peek();
+    }
+}
+
 
