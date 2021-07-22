c783639e6c4d2d1fadee90bd8b19193a8955e900
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-07-21 23:56:35 -0700

    1 hr for all solutions

--- maximum-subarray/maximum-subarray.java
+++ maximum-subarray/maximum-subarray.java
@@ -1,3 +1,4 @@
+// naive approach 20 mins O(n)
 class Solution {
     public int maxSubArray(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
@@ -13,5 +14,52 @@ class Solution {
             sum = 0;
         }
         return mx;
+    }
+}
+
+//cleaner
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int mx = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            mx = Math.max(mx, sum);
+        }
+        return mx;
+    }
+}
+
+//divide and conquer O(nlog(n))
+class Solution {
+    int[] nums;
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        this.nums = nums;
+        return helper(0, nums.length - 1);
+    }
+    
+    private int helper(int left, int right) {
+        if (left >= right) {
+            return nums[left];
+        }
+        int mid = left + (right - left) / 2;
+        int leftmax = helper(left, mid - 1);
+        int rightmax = helper(mid + 1, right);
+        int sum = 0;
+        int mxl = 0;
+        for (int i = mid - 1; i >= 0; i--) {
+            sum += nums[i];
+            mxl = Math.max(mxl, sum);
+        }
+        sum = 0;
+        int mxr = 0;
+        for (int i = mid + 1; i < nums.length; i++) {
+            sum += nums[i];
+            mxr = Math.max(mxr, sum);
+        }
+        int interme = Math.max(nums[mid] + mxr + mxl, leftmax);
+        return Math.max(interme, rightmax);
     }
 }
