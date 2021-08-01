8d1b55b8cd46cf54a5305d77b41ceb30c1f428d0
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-01 05:58:28 -0700

    Update maximum-length-of-pair-chain.java

--- maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -1,3 +1,4 @@
+// o(n^2)
 class Solution {
     public int findLongestChain(int[][] pairs) {
         if (pairs == null || pairs.length == 0) return 0;
@@ -14,5 +15,20 @@ class Solution {
         int result = 0;
         for (int i : memo) result = Math.max(i, result);
         return result;
+    }
+}
+
+//greedy algorithm, chain the smallest endTime, which can guarantee more chain in future
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        if (pairs == null || pairs.length == 0) return 0;
+        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+        int ans = 0;
+        int cur = -1001;
+        for (int[] pair : pairs) if (cur < pair[0]) {
+            cur = pair[1];
+            ans++;
+        }
+        return ans;
     }
 }
