7c6e747a3def70299e028c2c6c87b78e22248a0d
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-01 20:42:16 -0700

    20 mins

--- longest-common-prefix/longest-common-prefix.java
+++ longest-common-prefix/longest-common-prefix.java
@@ -1,18 +1,18 @@
+// vertical comparsion, O(nm), 20 mins
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
-        StringBuffer sb = new StringBuffer();
         int idx = 0;
-        while (true) {
-            if (strs[0].length() - 1 < idx) return sb.toString();
+        while (idx < strs[0].length()) {
+            char c = strs[0].charAt(idx);
             for (int i = 1; i < strs.length; i++) {
-                if (strs[i].length() - 1 < idx) return sb.toString();
-                if (strs[i].charAt(idx) != strs[i - 1].charAt(idx)) {
-                    return sb.toString();
+                if (idx > strs[i].length() - 1 || strs[i].charAt(idx) != c) {
+                    return strs[0].substring(0, idx);
                 }
             }
-            sb.append(strs[0].charAt(idx++));
+            idx++;
         }
+        return strs[0].substring(0, idx);
         
     }
 }
