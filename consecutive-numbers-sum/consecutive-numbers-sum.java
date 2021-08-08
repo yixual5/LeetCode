03a8a646b0583ecc6b564ea3bc567b090a3a7044
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-08 01:47:31 -0700

    40 mins

--- consecutive-numbers-sum/consecutive-numbers-sum.java
+++ consecutive-numbers-sum/consecutive-numbers-sum.java
@@ -1,8 +1,9 @@
+// 40 mins
 class Solution {
     public int consecutiveNumbersSum(int n) {
         int result = 0;
-        int upper_limit = (int)(Math.sqrt(2 * n + 0.25) - 0.5);
-        for (int k = 1; k <= upper_limit; ++k) {
+        int bound = (int) (Math.sqrt(2 * n + 0.25) - 0.5);
+        for (int k = 1; k <= bound; ++k) {
             n -= k;
             if (n % k == 0) result++;
         }
