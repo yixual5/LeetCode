ef08dbd09d4608149407383c03c6b870183b4afd
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-09 01:30:52 -0700

    Time: 1 ms (91.03%), Space: 38.8 MB (38.60%) - LeetHub

---
+++ string-compression/string-compression.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int compress(char[] chars) {
+        if (chars == null || chars.length == 0) return 0;
+        int left = 0;
+        int right = 0;
+        while (right < chars.length) {
+            char c = chars[right];
+            int start = right;
+            chars[left] = c;
+            while (right < chars.length && chars[right] == c) {
+                right++;
+            }
+            int count = right - start;
+            if (count != 1) {
+                List<Integer> l = new ArrayList<>();
+                while (count > 0) {
+                    l.add(count % 10);
+                    count /= 10;
+                }
+                for (int i = l.size() - 1; i >= 0; i--) {
+                    chars[++left] = (char)(48 + l.get(i));
+                }
+                
+            }
+            left++;
+            
+        }
+        return left;
+    }
+}
