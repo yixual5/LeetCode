a3e64b226cdb1920b10fc2a833a84cd50e8ceab2
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-09 19:45:27 -0700

    40 mins

--- minimum-knight-moves/minimum-knight-moves.java
+++ minimum-knight-moves/minimum-knight-moves.java
@@ -1,3 +1,4 @@
+// dfs, 40 mins
 class Solution {
     private Map<String, Integer> memo = new HashMap<>();
     
@@ -7,6 +8,9 @@ class Solution {
         
         if (x == 0 && y == 0) return 0;
         
+        // if x or y greater than 2, we can always reduced it below 2
+        // so (1, 0), (0 ,1), (1, 1), (2, 0), (0, 2) are all possible base case
+        // the first two can convert to the last two, so we check x + y == 2
         if (x + y == 2) return 2;
         
         int dis = Math.min(dfs(Math.abs(x - 1), Math.abs(y - 2)), 
