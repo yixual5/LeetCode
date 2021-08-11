dfd474214e0a3830072ba9b9da0986478fe61804
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-10 18:29:05 -0700

    Time: 22 ms (100.00%), Space: 45.6 MB (64.44%) - LeetHub

---
+++ shortest-word-distance-ii/shortest-word-distance-ii.java
@@ -0,0 +1,32 @@
+class WordDistance {
+    private Map<String, List<Integer>> map = new HashMap<>();
+    public WordDistance(String[] wordsDict) {
+        for (int i = 0; i < wordsDict.length; i++) {
+            map.computeIfAbsent(wordsDict[i], k -> new ArrayList<>()).add(i);
+        }
+    }
+    
+    public int shortest(String word1, String word2) {
+        List<Integer> l1 = map.get(word1);
+        List<Integer> l2 = map.get(word2);
+        int pointer1 = 0;
+        int pointer2 = 0;
+        int result = Integer.MAX_VALUE;
+        while (pointer1 < l1.size() && pointer2 < l2.size()) {
+            if (l1.get(pointer1) > l2.get(pointer2)) {
+                result = Math.min(Math.abs(l1.get(pointer1) - l2.get(pointer2++)), result);
+            } else if (l1.get(pointer1) < l2.get(pointer2)) {
+                result = Math.min(Math.abs(l1.get(pointer1++) - l2.get(pointer2)), result);
+            } else {
+                return 0;
+            }
+        }
+        return result;
+    }
+}
+
+/**
+ * Your WordDistance object will be instantiated and called as such:
+ * WordDistance obj = new WordDistance(wordsDict);
+ * int param_1 = obj.shortest(word1,word2);
+ */
