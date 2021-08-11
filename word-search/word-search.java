72a3f903e0c89062ebf560583d4d93b6aa233ef8
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-11 04:46:37 -0700

    30 min

--- word-search/word-search.java
+++ word-search/word-search.java
@@ -1,3 +1,4 @@
+// backtracking 0(N * 3^L)
 class Solution {
     char[] wd;
     char[][] board;
@@ -16,21 +17,20 @@ class Solution {
     }
     
     private boolean dfs(int idx, int i, int j) {
-        if (idx == wd.length - 1 && board[i][j] == wd[idx]) return true;
-        if (board[i][j] != wd[idx]) {
+        if (idx == wd.length) return true;
+        if (j < 0 || j >= board[0].length || i < 0 || i >= board.length || board[i][j] != wd[idx]) {
             return false;
         }   
-            char temp = board[i][j];
-            board[i][j] = '#';
+        board[i][j] = '#';
         
         for (int k = 0; k < 4; k++) {
             int col = j + x[k];
             int row = i + y[k];
-            if (col >= 0 && col < board[0].length && row >= 0 && row < board.length && board[row][col] != '#') {
-                if(dfs(idx + 1, row, col)) return true;
-            }
+            
+            if(dfs(idx + 1, row, col)) return true;
+            
         }
-        board[i][j] = temp;
+        board[i][j] = wd[idx];
         return false;
         
     }
