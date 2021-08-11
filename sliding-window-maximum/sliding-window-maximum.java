5161be0c7c392ce1979da05ec5c80e86cc40d492
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-11 02:54:02 -0700

    50 mins

--- sliding-window-maximum/sliding-window-maximum.java
+++ sliding-window-maximum/sliding-window-maximum.java
@@ -1,3 +1,4 @@
+// O(N)
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         if (nums == null || nums.length == 0 || k > nums.length || k == 0) return new int[]{0};
@@ -6,6 +7,7 @@ class Solution {
         int block = nums.length / k;
         left[0] = nums[0];
         right[nums.length - 1] = nums[nums.length - 1];
+        // start from 1 because not right is not aligned
         for (int i = 1; i < nums.length; i++) {
             if (i % k == 0) left[i] = nums[i];
             else left[i] = Math.max(left[i - 1], nums[i]);
@@ -14,6 +16,7 @@ class Solution {
             if ((j + 1) % k == 0) right[j] = nums[j];
             else right[j] = Math.max(right[j + 1], nums[j]);
         }
+        // block i to j: max of right to left of i + max of left to right of j
         int[] result = new int[nums.length - k + 1];
         for (int i = 0; i < result.length; i++) {
             result[i] = Math.max(right[i], left[i + k - 1]);
