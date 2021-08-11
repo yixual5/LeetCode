ae7d7ee25139ccdc99d1e19c34101290f0682b74
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-10 20:36:50 -0700

    40 mins

--- serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -7,39 +7,48 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+// O(n), dfs preorder
 public class Codec {
     StringBuffer sb = new StringBuffer();
-    int idx = 0;
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
+        StringBuffer sb = new StringBuffer();
+        dfs(sb, root);
+        return sb.toString();
+        
+    }
+    
+    private void dfs(StringBuffer sb, TreeNode root) {
         if (root == null) {
             sb.append("null,");
-            return sb.toString();
+            return;
         }
-        sb.append(root.val + ",");
-        serialize(root.left);
-        serialize(root.right);
-        return sb.toString();
+        sb.append(root.val).append(',');
+        dfs(sb, root.left);
+        dfs(sb, root.right);
     }
-
+    
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         String[] temp = data.split(",");
-        return helper(temp);
+        int index[] = new int[]{-1};
+        return helper(temp,index);
     }
     
-    private TreeNode helper(String[] temp) {
-        if (idx >= temp.length) {
+    private TreeNode helper(String[] temp, int[] index) {
+        index[0]++;
+        
+        if (index[0] >= temp.length) {
             return null;
         }
-        if (temp[idx].equals("null")) {
-            idx++;
+        if (temp[index[0]].equals("null")) {
             return null;
         }
         
-        TreeNode root = new TreeNode(Integer.valueOf(temp[idx++]));
-        root.left = helper(temp);
-        root.right = helper(temp);
+        TreeNode root = new TreeNode(Integer.valueOf(temp[index[0]]));
+        root.left = helper(temp, index);
+        root.right = helper(temp, index);
         return root;
     }
 }
