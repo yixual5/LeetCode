68a3c190f81ede53846bf7d04df4c8022d3cb63e
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-13 02:35:05 -0700

    Update peeking-iterator.java

--- peeking-iterator/peeking-iterator.java
+++ peeking-iterator/peeking-iterator.java
@@ -39,3 +39,44 @@ class PeekingIterator implements Iterator<Integer> {
         return peek != null || itr.hasNext();
 	}
 }
+
+
+// get peek at constructor
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+import java.util.NoSuchElementException;
+class PeekingIterator implements Iterator<Integer> {
+    private Iterator<Integer> itr;
+    private Integer next = null;
+    
+	public PeekingIterator(Iterator<Integer> iterator) {
+        if (!iterator.hasNext()) throw new NoSuchElementException();
+        next = iterator.next();
+	    itr = iterator;
+	    
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+        return next;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+        if (next == null) throw new NoSuchElementException();
+        Integer temp = next;
+        next = null;
+        if (itr.hasNext()) {
+            next = itr.next();
+        }
+        return temp;
+        
+	}
+	
+	@Override
+	public boolean hasNext() {
+        return next != null;
+	}
+}
