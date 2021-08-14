69472cc3ee3c41fd141e65f18d252728bf20ba32
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-13 19:13:02 -0700

    30 mins

--- high-five/high-five.java
+++ high-five/high-five.java
@@ -1,3 +1,4 @@
+//priority queue to keep first 5
 class Solution {
     public int[][] highFive(int[][] items) {
         Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
@@ -15,5 +16,34 @@ class Solution {
             result[idx++][1] =(int) (temp / 5.0);
         }
         return result;
+    }
+}
+
+//sort first
+class Solution {
+    public int[][] highFive(int[][] items) {
+        Arrays.sort(items, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                if (a[0] == b[0]) {
+                    return b[1] - a[1];
+                } else {
+                    return a[0] - b[0];
+                }
+            }
+        });
+        List<int[]> result = new ArrayList<>();
+        for (int i = 0; i < items.length; i++) {
+            int id = items[i][0];
+            int count = 0;
+            for (int j = i; j < i + 5; j++) {
+                count += items[j][1];
+            }
+            result.add(new int[]{id, count / 5});
+            while (i < items.length && items[i][0] == id) i++;
+            i--;
+        }
+        int[][] solution = new int[result.size()][];
+        return result.toArray(solution);
     }
 }
