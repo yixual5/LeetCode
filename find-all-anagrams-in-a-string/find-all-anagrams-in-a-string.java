170e25016d447c504b6146c61a25f37d46e7148a
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-15 00:46:25 -0700

    40 mins

--- find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -28,3 +28,27 @@ class Solution {
         return result;
     }
 }
+
+//simpler method, consider all size p.length() window
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || p == null || s.length() == 0 || p.length() == 0) return result;
+        int[] count = new int[26];
+        for (char c : p.toCharArray()) {
+            count[c - 'a']++;
+        }
+        int[] count2 = new int[26];
+        int right = 0;
+        while (right < s.length()) {
+            
+            count2[s.charAt(right) - 'a']++;
+            if (right >= p.length()) {
+                count2[s.charAt(right - p.length()) - 'a']--;
+            }
+            if (Arrays.equals(count, count2)) result.add(right - p.length() + 1);
+            right++;
+        }
+        return result;
+    }
+}
