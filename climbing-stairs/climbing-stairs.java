13b2a54a06f308bae6393f773d49ee9d7a40fcf1
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-16 05:12:52 -0700

    Time: 0 ms (100.00%), Space: 36 MB (18.20%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 0) return 0;
+        int[] memo = new int[n + 1];
+        memo[0] = 1;
+        memo[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+        return memo[n];
+    }
+}
