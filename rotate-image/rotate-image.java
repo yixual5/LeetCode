ab37e723aba2f289e36064a600a6561be3ff78ee
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-15 20:02:51 -0700

    1 hr

--- rotate-image/rotate-image.java
+++ rotate-image/rotate-image.java
@@ -1,5 +1,7 @@
+// cut the matrix into 4 part, rotate each part, O(N^2)
 class Solution {
     public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return;
         int n = matrix.length;
         for (int i = 0; i < (n + 1) / 2; i++) { // include mid
             for (int j = 0; j < n / 2; j++) { // not include mid
@@ -8,6 +10,35 @@ class Solution {
                 matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
                 matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
                 matrix[j][n - 1 - i] = temp;
+            }
+        }
+    }
+}
+
+//transpose first, then reflect
+class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return;
+        transpose(matrix);
+        reflect(matrix);
+    }
+    
+    private void transpose(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    
+    private void reflect(int[][] matrix) {
+         for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].length - 1 - j];
+                matrix[i][matrix[0].length - 1 - j] = temp;
             }
         }
     }
