cfacbd22fa1b48bfab3f784d11b20ee9ba73e215
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-17 01:53:54 -0700

    1 hr

--- accounts-merge/accounts-merge.java
+++ accounts-merge/accounts-merge.java
@@ -1,31 +1,39 @@
 // Union find
 class Solution {
-    int[] parent = new int[10001];
+    int[] parent;
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        parent = new int[accounts.size()];
         for (int i = 0; i < parent.length; i++) {
             parent[i] = i;
         }
-        Map<String, String> emailToName = new HashMap<>();
         Map<String, Integer> emailToId = new HashMap<>();
         int id = 0;
-        for (List<String> s : accounts) {
-            String name = s.remove(0);
-            for (String email : s) {
-                emailToName.put(email, name);
-                if (!emailToId.containsKey(email)) emailToId.put(email, id++);
-                union(emailToId.get(s.get(0)), emailToId.get(email));
+        for (int i = 0; i < accounts.size(); i++) {
+            List<String> emailList = accounts.get(i);
+            for (int j = 1; j < emailList.size(); j++) {
+                if (!emailToId.containsKey(emailList.get(j))) emailToId.put(emailList.get(j), i);
+                else union(i, emailToId.get(emailList.get(j)));
             }
         }
-        Map<Integer, List<String>> cluster = new HashMap<>();
-        for (String email : emailToId.keySet()) {
-            cluster.computeIfAbsent(find(emailToId.get(email)), k -> new ArrayList<>()).add(email);
-        }
         List<List<String>> result = new ArrayList<>();
-        for (Integer i : cluster.keySet()) {
-            List<String> temp = cluster.get(i);
-            Collections.sort(temp);
-            temp.add(0, emailToName.get(temp.get(0)));
-            result.add(temp);
+        //for (int i = 0; i < accounts.size(); i++) result.add(new ArrayList<>());
+        Map<Integer, Integer> cluster = new HashMap<>();
+        int idx = 0;
+        for (String email : emailToId.keySet()) {
+            int eid = find(emailToId.get(email));
+            if (!cluster.containsKey(eid)) {
+                cluster.put(eid, idx);
+                List<String> temp = new ArrayList<>();
+                temp.add(email);
+                result.add(temp);
+                idx++;
+            } else {
+                result.get(cluster.get(eid)).add(email);
+            }
+        }
+        result.forEach(k -> Collections.sort(k));
+        for (List<String> i : result) {
+            i.add(0, accounts.get(emailToId.get(i.get(0))).get(0));
         }
         return result;
     }
