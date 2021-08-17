66d8234c7f01325946446611eef0d54482a02dc3
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-16 21:01:15 -0700

    30 mins

--- gas-station/gas-station.java
+++ gas-station/gas-station.java
@@ -1,3 +1,4 @@
+// if total > 0, at some point we must have a positive cur so that we can balance the prev negativ, O(N)
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int cur = 0;
@@ -12,5 +13,22 @@ class Solution {
             }
         }
         return total < 0 ? -1 : idx;
+    }
+}
+
+// same idea
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0;
+        int idx = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+            if (total < min) {
+                idx = i;
+                min = total;
+            }
+        }
+        return total < 0 ? -1 : (idx + 1) % gas.length;
     }
 }
