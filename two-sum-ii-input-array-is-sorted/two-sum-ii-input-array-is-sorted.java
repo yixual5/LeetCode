7b44360d08709e09db64707f58160bffd549a9e1
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-17 20:14:47 -0700

    30 min

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -10,3 +10,19 @@ class Solution {
         return new int[]{0, 0};
     }
 }
+
+//O(log(n)) average
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int sum = numbers[left] + numbers[right];
+            if (target == sum) return new int[]{left + 1, right + 1};
+            else if (target < sum) right = (numbers[left] + numbers[mid] > target) ? mid - 1 : right - 1;
+            else left = (numbers[right] + numbers[mid] < target) ? mid + 1 : left + 1;
+        }
+        return new int[]{0, 0};
+    }
+}
