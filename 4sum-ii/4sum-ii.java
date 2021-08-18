36bacd27df8f711b0ac3732255d24ebcb4668554
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-18 00:26:23 -0700

    1 hr

--- 4sum-ii/4sum-ii.java
+++ 4sum-ii/4sum-ii.java
@@ -15,3 +15,36 @@ class Solution {
         return result;
     }
 }
+
+//generalized sol
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int[][] input = {nums1, nums2, nums3, nums4};
+        add(count, 0, 0, input);
+        return countc(count, input.length / 2, 0, input);
+    }
+    
+    private void add(Map<Integer, Integer> count, int idx, int sum, int[][] input){
+        if (idx == input.length / 2) {
+            count.put(sum, count.getOrDefault(sum, 0) + 1);
+        } else {
+            for (int a : input[idx]) {
+                add(count, idx + 1, sum + a, input);
+            }
+        }
+    }
+    
+    private int countc(Map<Integer, Integer> count, int idx, int sum, int[][] input) {
+        int cot = 0;
+        if (idx == input.length) {
+            return count.getOrDefault(sum, 0);
+        } else {
+            
+            for (int a : input[idx]) {
+                cot += countc(count, idx + 1, sum - a, input);
+            }
+        }
+        return cot;
+    }
+}
