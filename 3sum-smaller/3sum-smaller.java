40c0fd13bd00be41f0ff25ad7f40024ba2586d21
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-18 02:14:34 -0700

    20 min

--- 3sum-smaller/3sum-smaller.java
+++ 3sum-smaller/3sum-smaller.java
@@ -1,9 +1,11 @@
+//O(N^2)
 class Solution {
     public int threeSumSmaller(int[] nums, int target) {
         if (nums == null || nums.length == 0) return 0;
         int count = 0;
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] * 3 >= target) break;
             int left = i + 1;
             int right = nums.length - 1;
             while (left < right) {
