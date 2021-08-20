97078e44eb51ebae25ac6fb37192f5dba61f23c6
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-19 22:12:30 -0700

    1 hr

--- maximal-square/maximal-square.java
+++ maximal-square/maximal-square.java
@@ -1,3 +1,4 @@
+// copy from maximal-rectangle
 class Solution {
     public int maximalSquare(char[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
@@ -33,5 +34,27 @@ class Solution {
             }
         }
         return max;
+    }
+}
+
+// dp, three same size square with a right corner can be a n + 1 square
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        int max = 0, prev = 0;
+        int[] dp = new int[matrix[0].length + 1];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 1; j < dp.length; j++) {
+                int temp = dp[j];
+                if (matrix[i][j - 1] == '1') {
+                    dp[j] = Math.min(Math.min(dp[j], dp[j - 1]), prev) + 1;
+                    max = Math.max(dp[j], max);
+                } else {
+                    dp[j] = 0;
+                }
+                prev = temp;
+            }
+        }
+        return max * max;
     }
 }
