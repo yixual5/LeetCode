b28a0454cbccbb1aa5b90eb006ffcb466652b5bb
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-19 19:41:28 -0700

    1 hr

--- largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -15,3 +15,35 @@ class Solution {
         return max;
     }
 }
+
+// using two array to simulate the function of stack, same theoritcal runtime but in practice faster
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) return 0;
+        int[] leftsmaller = new int[heights.length];
+        int[] rightsmaller = new int[heights.length];
+        leftsmaller[0] = -1;
+        rightsmaller[heights.length - 1] = heights.length;
+        for (int i = 1; i < heights.length; i++) {
+            int prev = i - 1;
+            // find the prev smaller left value immediately before i
+            while (prev >= 0 && heights[i] <= heights[prev]) {
+                prev = leftsmaller[prev];
+            }
+            leftsmaller[i] = prev;
+        }
+        for (int i = heights.length - 2; i >= 0; i--) {
+            int prev = i + 1;
+            // find the prev smaller right value immediately after i
+            while (prev < heights.length && heights[i] <= heights[prev]) {
+                prev = rightsmaller[prev];
+            }
+            rightsmaller[i] = prev;
+        }
+        int max = 0;
+        for (int i = 0; i < heights.length; i++) {
+            max = Math.max(max, (rightsmaller[i] - leftsmaller[i] - 1) * heights[i]);
+        }
+        return max;
+    }
+}
