344f6e0e992015fb376fe2aa811aaa896689610f
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-22 00:24:32 -0700

    Time: 1 ms (99.82%), Space: 36.2 MB (90.93%) - LeetHub

---
+++ last-stone-weight-ii/last-stone-weight-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+        if (stones == null || stones.length == 0) return 0;
+        int sum = 0;
+        for (int stone : stones) sum += stone;
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int i = 1; i <= stones.length; i++) {
+            for (int j = target; j >= stones[i - 1]; j--) {
+                dp[j] |= dp[j - stones[i - 1]]; 
+            }
+        }
+        int result = 0;
+        for (int i = 0; i <= target; i++) {
+            if (dp[i]) result = i;
+        }
+        
+        return sum - 2*result;
+    }
+}
