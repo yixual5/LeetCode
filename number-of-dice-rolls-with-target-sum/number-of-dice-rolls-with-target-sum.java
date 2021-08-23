338b03c195da8e1f246974f53360cb290005763c
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-23 05:10:13 -0700

    1 hr

--- number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -20,3 +20,29 @@ class Solution {
         return dp[target];
     }
 }
+
+// some optimization
+class Solution {
+    public int numRollsToTarget(int d, int f, int target) {
+        if (d * f < target) return 0;
+        int[][] dp = new int[d + 1][target + 1];
+        dp[0][0] = 1;
+        int mod = (int) 1e+9 + 7;
+        
+        for (int i = 1; i <= d; i++) {
+            
+            for (int j = i; j <= target; j++) {
+                if(i * f < j){
+                    break;
+                }
+                for (int k = 1; k <= f; k++) {
+                    if (j - k < 0) continue;
+                    dp[i][j] += dp[i - 1][j - k];
+                    dp[i][j] %= mod;
+                }
+            }
+            
+        }
+        return dp[d][target];
+    }
+}
