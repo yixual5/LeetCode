7e45b0f2688cb4c123cbafa18016ddcd56235b7f
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-25 04:32:15 -0700

    30 mins

--- design-hit-counter/design-hit-counter.java
+++ design-hit-counter/design-hit-counter.java
@@ -25,3 +25,39 @@ class HitCounter {
  * obj.hit(timestamp);
  * int param_2 = obj.getHits(timestamp);
  */
+
+//follow up : hit many times in a timestamp, which can reduce the size of queue
+class HitCounter {
+    private Deque<int[]> q = new LinkedList<>();
+    private int total = 0;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        if (!q.isEmpty() && q.getLast()[0] == timestamp) q.getLast()[1]++;
+        else q.add(new int[]{timestamp, 1});
+        total++;
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        while (!q.isEmpty() && timestamp - q.getFirst()[0] >= 300) {
+            int[] temp = q.removeFirst();
+            total -= temp[1];
+        }
+            
+        return total;
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
