764afe0caa97800efb7ead2e6807cd605570aeed
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-26 00:28:25 -0700

    Time: 2 ms (98.66%), Space: 40.6 MB (19.17%) - LeetHub

--- task-scheduler/task-scheduler.java
+++ task-scheduler/task-scheduler.java
@@ -1,14 +1,21 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
+        int max = 0;
+        int maxCount = 0;
         for (char task : tasks) {
             freq[task - 'A']++;
+            if (freq[task - 'A'] == max) {
+                maxCount++;
+            } else if (freq[task - 'A'] > max) {
+                max = freq[task - 'A'];
+                maxCount = 1;
+            }
         }
-        Arrays.sort(freq);
-        int idle = (freq[freq.length - 1] - 1) * n;
-        for (int i = freq.length - 2; i >= 0 && freq[i] > 0; i--) {
-            idle -= Math.min(freq[freq.length - 1] - 1, freq[i]);
-        }
-        return Math.max(0, idle) + tasks.length;
+        //if maxCount > 1, we need reduce the n by corresponding amount
+
+        int emptySlot = (n - maxCount + 1) * (max - 1);
+        int remain = tasks.length - maxCount * max;
+        return Math.max(0, emptySlot - remain) + tasks.length;
     }
 }
