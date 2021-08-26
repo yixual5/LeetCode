ad93d4bf6a115aeb48d3b07d4ab6183d4b98c70b
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-26 01:40:18 -0700

    1 hr

--- number-of-provinces/number-of-provinces.java
+++ number-of-provinces/number-of-provinces.java
@@ -1,3 +1,4 @@
+//dfs
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         boolean[] visited = new boolean[isConnected.length];
@@ -21,4 +22,57 @@ class Solution {
             }
         }
     }
+}
+
+//union find with rank and compression
+class Solution {
+    class UnionFind{
+        int[] rank;
+        int[] parent;
+        int count;
+        public UnionFind(int n) {
+            count = n;
+            rank = new int[n];
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+        
+        public int find(int i) {
+            while (parent[i] != i) {
+                //compression
+                parent[i] = parent[parent[i]];
+                i = parent[i];
+            }
+            return i;
+        }
+        
+        public void union(int i, int j) {
+            int rooti = find(i);
+            int rootj = find(j);
+            if (rooti == rootj) return;
+            if (rank[rooti] > rank[rootj]) {
+                rank[rooti] += rank[rootj];
+                parent[rootj] = parent[rooti];
+            } else {
+                rank[rootj] += rank[rooti];
+                parent[rooti] = parent[rootj];
+            }
+            count--;
+        }
+        
+    }
+    
+    public int findCircleNum(int[][] isConnected) {
+        UnionFind uf = new UnionFind(isConnected.length);
+        for (int i = 0; i < isConnected.length; i++) {
+            for (int j = i + 1; j < isConnected[0].length; j++) {
+                if (isConnected[i][j] == 1) uf.union(i, j);
+            }
+        }
+        return uf.count;
+    }
+    
 }
