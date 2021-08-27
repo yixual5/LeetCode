1210a1fc9c222926c56e6eee3d126e99d659ff01
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-08-26 19:17:47 -0700

    40 mins

--- integer-break/integer-break.java
+++ integer-break/integer-break.java
@@ -1,3 +1,4 @@
+//factor 2 and 3 works best, math
 class Solution {
     public int integerBreak(int n) {
         if (n == 2) return 1;
@@ -5,5 +6,19 @@ class Solution {
         if (n % 3 == 0) return (int) Math.pow(3, n / 3);
         if (n % 3 == 2) return 2 * (int) Math.pow(3, (n - 2) / 3);
         return 4 * (int) Math.pow(3, (n - 4) / 3);
+    }
+}
+
+//dp
+class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i / 2; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j, dp[j]) * Math.max(i - j, dp[i - j]));
+            }
+        }
+        return dp[n];
     }
 }
