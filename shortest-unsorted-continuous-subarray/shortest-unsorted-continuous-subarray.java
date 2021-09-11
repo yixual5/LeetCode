4ccb2e84bcac7013675ad9006f20784d1e1dacdc
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-09-10 18:36:28 -0700

    1 hr

--- shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
+++ shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
@@ -1,3 +1,5 @@
+//from left, find most left position to start
+//from right, find most right poision to end
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
         Deque<Integer> s1 = new ArrayDeque<>();
@@ -14,5 +16,22 @@ class Solution {
             s2.push(nums.length - i - 1);
         }
         return lo > high ? 0 : high - lo + 1;
+    }
+}
+
+//same idea
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        if (nums == null || nums.length == 0 || nums.length == 1) return 0;
+        int hi = -2, lo = -1;
+        int min = nums[nums.length - 1];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+            hi = max > nums[i] ? i : hi;
+            min = Math.min(min, nums[nums.length - i - 1]);
+            lo = min < nums[nums.length - i - 1] ? nums.length - 1 - i : lo;
+        }
+        return hi - lo + 1;
     }
 }
