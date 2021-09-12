6add8df21ab7fba35be9695a714359bd9b18813d
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-09-12 10:39:03 -0700

    1 hr

--- remove-duplicate-letters/remove-duplicate-letters.java
+++ remove-duplicate-letters/remove-duplicate-letters.java
@@ -21,3 +21,27 @@ class Solution {
         return sb.reverse().toString();
     }
 }
+
+//using stringbuffer as a stack
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) count[c - 'a']++;
+        boolean[] visited = new boolean[26];
+        StringBuffer sb = new StringBuffer();
+        for (char c : s.toCharArray()) {
+            count[c - 'a']--;
+            if (!visited[c - 'a']) {
+                while (sb.length() > 0 && c < sb.charAt(sb.length() - 1) && count[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                    visited[sb.charAt(sb.length() - 1) - 'a'] = false;
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+                visited[c - 'a'] = true;
+                sb.append(c);
+                
+            }
+            
+        }
+        return sb.toString();
+    }
+}
