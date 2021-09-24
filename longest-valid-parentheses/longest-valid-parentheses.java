d96ae04d747dc40d4e199efb053d32893f3fee63
yixual5 <62257975+yixual5@users.noreply.github.com>
2021-09-23 18:45:08 -0700

    1 hr

--- longest-valid-parentheses/longest-valid-parentheses.java
+++ longest-valid-parentheses/longest-valid-parentheses.java
@@ -18,3 +18,23 @@ class Solution {
         return mx == Integer.MIN_VALUE ? 0 : mx;
     } 
 }
+
+//stack
+class Solution {
+    public int longestValidParentheses(String s) {
+        if (s == null || s.length() == 0) return 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+        int mx = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) stack.push(i);
+                else mx = Math.max(i - stack.peek(), mx);
+            }
+        }
+        return mx;
+    } 
+}
