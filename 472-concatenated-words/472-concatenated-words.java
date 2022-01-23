b96a2100bbb9efb5f10eef99f8908cf39e1a4454
yixual5 <62257975+yixual5@users.noreply.github.com>
2022-01-22 19:09:37 -0500

    Time: 2304 ms (5.01%), Space: 174.9 MB (22.38%) - LeetHub

---
+++ 472-concatenated-words/472-concatenated-words.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Arrays.sort(words, (a1, a2) -> a1.length() - a2.length());
+        Set<String> dict = new HashSet<>();
+        List<String> res = new LinkedList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (wordBreak(words[i], dict)) res.add(words[i]);
+            dict.add(words[i]);
+        }
+        return res;
+    }
+    
+    private boolean wordBreak(String s, Set<String> dict) {
+        if (dict.isEmpty()) return false;
+        // end at index - 1
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
+                    dp[i] = true; 
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
