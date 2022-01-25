e831a36bda04ed4de8cadcc0b6d4b000a038398b
yixual5 <62257975+yixual5@users.noreply.github.com>
2022-01-25 11:55:26 -0500

    Time: 69 ms (32.68%), Space: 132.8 MB (14.34%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a1, a2) -> nums[a2] - nums[a1]);
-        for (int i = 0; i < k; i++) pq.add(i);
+        Deque<Integer> dq = new ArrayDeque<>();
         int[] res = new int[nums.length - k + 1];
-        res[0] = nums[pq.peek()];
-        for (int i = k; i < nums.length; i++) {
-            pq.add(i);
-            while (!pq.isEmpty() && i - pq.peek() >= k) pq.remove();
-            res[i - k + 1] = nums[pq.peek()];
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (!dq.isEmpty() && i - dq.getFirst() >= k) dq.poll();
+            while (!dq.isEmpty() && nums[dq.getLast()] < nums[i]) dq.removeLast();
+            dq.addLast(i);
+            if (i - k + 1 >= 0) res[i - k + 1] = nums[dq.getFirst()];
         }
         return res;
     }
